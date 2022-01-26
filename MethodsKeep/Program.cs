f8bd4b5dd916ccedd3f326dfb262e2ee8d316756
JoeH-GD <joe.haldon.gd@gmail.com>
2022-01-27 00:38:13 +0300

    FreeCoding

---
+++ MethodsKeep/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsKeep
+{
+    class MethodKeep
+    {
+        //Печатает строчку по заданным координатам
+        static void PrintLinesCustom(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+
+        //Находит скалярное произведение векторов в трехмерном пространстве
+        static int ScalarVector3D(int aX, int aY, int aZ, int bX, int bY, int bZ)
+        {
+            int scalar = (aX * bX) + (aY * bY) + (aZ * bZ);
+            return scalar;
+
+        }
+
+        //Ну а вдруг надо поменять размер массива...
+        static int[] ChangeArrayLength(int[] n, int newLength)
+        {
+          int length = 0;
+          length = n.Length;
+          Array.Resize(ref n, newLength);
+         length = n.Length;
+        return n;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
